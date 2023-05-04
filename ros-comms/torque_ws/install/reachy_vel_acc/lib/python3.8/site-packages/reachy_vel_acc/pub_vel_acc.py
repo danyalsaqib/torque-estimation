0fc51da09ea487dedde7717f840e94f030b24f56
danyalsaqib <dsaqib.bee17seecs@seecs.edu.pk>
2023-05-04 16:55:28 -0600

    Added moving average smoothing

--- ros-comms/torque_ws/install/reachy_vel_acc/lib/python3.8/site-packages/reachy_vel_acc/pub_vel_acc.py
+++ ros-comms/torque_ws/install/reachy_vel_acc/lib/python3.8/site-packages/reachy_vel_acc/pub_vel_acc.py
@@ -1,7 +1,7 @@
 import rclpy
 import numpy as np
 
-import os.path 
+import os
 
 from rclpy.node import Node
 
@@ -9,10 +9,10 @@ from std_msgs.msg import String, Float64MultiArray
 from sensor_msgs.msg import JointState
 from torque_msgs.msg import ExpandedJointState
 
-class MinimalSubscriber(Node):
+class ExpandedStatePublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('expanded_state_publisher')
         self.subscription = self.create_subscription(
             JointState,
             'joint_states',
@@ -20,28 +20,14 @@ class MinimalSubscriber(Node):
             qos_profile=5,
         )
         
-        # self.subscription  # prevent unused variable warning
         self.expanded_joint_state_publisher = self.create_publisher(
             msg_type=ExpandedJointState,
             topic='expanded_joint_states',
             qos_profile=5,
         )
 
-        #self.joint_state_publisher = self.create_publisher(
-        #    msg_type=Float64MultiArray,
-        #    topic='expanded_joint_states',
-        #    qos_profile=10,
-        #)
+        self.delete_prev_save_files()
 
-        # self.expanded_joint_state = JointState()
-    
-    #def unpack_joint_state(self, joint_array):
-    #    ret_array = Float64MultiArray()
-    #
-    #    for val in joint_array:
-    #        ret_array.data.append(float(val))
-    #
-    #    return ret_array
 
     def unpack_joint_state(self, joint_array):
         ret_array = np.array([])
@@ -51,12 +37,44 @@ class MinimalSubscriber(Node):
 
         return ret_array
 
+
     def unpack_time_state(self, time_array):
         time_value = time_array.sec + (time_array.nanosec / 1000000000)
         return time_value
 
+
+    def moving_average(self, pos, win_file_name, win_index_name, win_size=8):
+        if (os.path.isfile(win_file_name)):
+            pos_window = np.load(win_file_name)
+        else:
+            pos_window = np.zeros([win_size, len(pos)])
+
+        if (os.path.isfile(win_index_name)):
+            index = np.load(win_index_name)
+        else:
+            index = np.array([0])
+
+        #print(size(pos_window))
+        #print(index[0])
+        
+        pos_window[index[0]] = pos
+        moving_avg = np.sum(pos_window, axis=0)
+        moving_avg = moving_avg / win_size
+
+        if index[0] == win_size - 1:
+            index[0] = 0
+        else:
+            index[0] = index[0] + 1
+
+        np.save(win_file_name, pos_window)
+        np.save(win_index_name, index)
+
+        return moving_avg
+
+
     def get_velocity_and_acceleration(self, pos_array, time_array):
         curr_pos = self.unpack_joint_state(pos_array)
+        curr_pos = self.moving_average(curr_pos, win_file_name='pos_window.npy', win_index_name = 'pos_window_index.npy', win_size = 32)
         curr_time = self.unpack_time_state(time_array)
         
         if (os.path.isfile('prev_pos.npy')):
@@ -73,6 +91,7 @@ class MinimalSubscriber(Node):
         d_time = curr_time - prev_time
 
         curr_vel = d_pos / d_time
+        curr_vel = self.moving_average(curr_vel, win_file_name='vel_window.npy', win_index_name = 'vel_window_index.npy', win_size = 16)
 
         if (os.path.isfile('prev_vel.npy')):
             prev_vel = np.load('prev_vel.npy')
@@ -88,7 +107,9 @@ class MinimalSubscriber(Node):
         np.save('prev_vel.npy', curr_vel)
 
         curr_vel = curr_vel.tolist()
+        curr_vel = [float(0) if x <= 0.001 else x for x in curr_vel]
         curr_acc = curr_acc.tolist()
+        curr_acc = [float(0) if x <= 0.001 else x for x in curr_acc]
 
         curr_vel_arr = Float64MultiArray()
         curr_vel_arr.data = curr_vel
@@ -98,12 +119,36 @@ class MinimalSubscriber(Node):
 
         return curr_vel_arr, curr_acc_arr
 
+
     def copy_joint_state_to_expanded(self, msg):
         expanded_joint_state = ExpandedJointState()
         expanded_joint_state.header = msg.header
         expanded_joint_state.name = msg.name
         expanded_joint_state.position = msg.position
         return expanded_joint_state
+
+    def delete_prev_save_files(self):
+        if (os.path.isfile('pos_window.npy')):
+            os.remove('pos_window.npy')
+
+        if (os.path.isfile('pos_window_index.npy')):
+            os.remove('pos_window_index.npy')
+
+        if (os.path.isfile('vel_window.npy')):
+            os.remove('vel_window.npy')
+
+        if (os.path.isfile('vel_window_index.npy')):
+            os.remove('vel_window_index.npy')
+        
+        if (os.path.isfile('prev_pos.npy')):
+            os.remove('prev_pos.npy')
+        
+        if (os.path.isfile('prev_time.npy')):
+            os.remove('prev_time.npy')
+        
+        if (os.path.isfile('prev_vel.npy')):
+            os.remove('prev_vel.npy')
+
 
     def listener_callback(self, msg):
         self.expanded_joint_state = self.copy_joint_state_to_expanded(msg)
@@ -114,44 +159,17 @@ class MinimalSubscriber(Node):
         self.expanded_joint_state_publisher.publish(self.expanded_joint_state)
 
 
-
-
-        #position_array = self.unpack_joint_state(msg.position)
-        #time_value = self.unpack_time_state(msg.header.stamp)
-        #self.get_logger().info('Publishing: "%s"' % position_array)
-        #self.joint_state_publisher.publish(position_array)
-
-        #self.to_be_published_expanded_joint_state = JointState()
-        #self.to_be_published_expanded_joint_state.header = msg.header
-        #self.get_logger().info('Publishing: "%s"' % self.to_be_published_expanded_joint_state.header.stamp.sec)
-        #self.get_logger().info('Publishing: "%s"' % self.to_be_published_expanded_joint_state.header.stamp.nanosec)
-        
-        #self.joint_state_publisher.publish(self.expanded_joint_state)
-
-        #for i, name in enumerate(msg.name):
-        #    if msg.name:
-        #        self.expanded_joint_state.name[i] = msg.name[i]
-        #    if msg.position:
-        #        self.expanded_joint_state.position[i] = msg.position[i]
-        #    if msg.velocity:
-        #    if msg.effort:
-        #        self.expanded_joint_state.effort[i] = msg.effort[i]
-
-        #self.get_logger().info('Publishing: "%s"' % self.expanded_joint_state)
-
-
-
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    expanded_state_publisher = ExpandedStatePublisher()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(expanded_state_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    expanded_state_publisher.destroy_node()
     rclpy.shutdown()
 
 
